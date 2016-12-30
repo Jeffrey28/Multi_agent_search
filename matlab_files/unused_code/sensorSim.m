0eaab556d193c736abed0a6592ed2d606f0c17aa
shionlau <shionlau@gmail.com>
2016-12-30 15:36:57 -0800

    removed unused files

--- matlab_files/unused_code/sensorSim.m
+++
@@ -1,9 +0,0 @@
-function z = sensorSim(rbt,fld)
-x_r = [fld.x;fld/y];
-t = [fld.tx;fld.ty];
-inv_cov = rbt.inv_sen_cov;
-offset = rbt.offset;
-
-prob = exp(-1/2*(t+offset-x_r)'*inv_cov*(t+offset-x_r));
-z = (rand(1,1) < prob);
-end
